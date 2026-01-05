95bd543457bf507ea22dd39def96064dfbcd40b5
tarunmakwana2211 <tarunmakwana0001@gmail.com>
2026-01-05 10:50:15 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,24 @@
+#str type
+"""x = "hello world"
+print(x)"""
+#float type
+"""x = 20.5
+print(x)"""
+#complex type
+"""x = 1j
+print(x)"""
+#bool type
+"""x = True
+print(x)"""
+#list type
+"""x = ["apple","banana","cherry"]
+print(x)"""
+#tupel type
+"""x = ["apple","banana","cherry"]
+print(x)"""
+#dict type
+"""x = ["name:","john","age:",36]
+print(x)"""
+#none type
+ x = ?
+ print(x)
