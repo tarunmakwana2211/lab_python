3f2ec59abf9ebaf4bb976e256ab8a0a6ec56b01c
tarunmakwana2211 <tarunmakwana0001@gmail.com>
2026-01-19 10:45:15 +0530

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,66 @@
+#Q1
+"""print("welcome to python programming")"""
+
+#Q2
+"""print("Rajesh Kumar")
+print("\tFlat No. 101. sunshine apartments")
+print("\t MG Road. sector 15")
+print("\tRajkot")
+print("\nPincode:\t360004")
+print("\nindia")"""
+
+#Q3
+"""a = 150
+b = 120.50
+print("addition:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)"""
+
+#Q4
+"""radius = 7
+pi = 3.14
+area = pi*radius*radius
+circumference = 2*pi*radius
+print("Area of Circle:",area)
+print("Circumference of Circle:",circumference)"""
+
+#Q5
+"""P = 5000
+R = 5
+T = 2
+simple_interest = (P*R*T)/100
+print("Simple Interest:",simple_interest)"""
+
+#Q6
+"""length = 10
+width = 5
+perimeter = 2 * (length+width)
+print("Perimeter of Rectangle:",perimeter)"""
+
+#Q7
+"""length = 10
+width = 5
+area = length*width
+perimeter = 2*(length+width)
+print("Area of Rectangle:",area)
+print("Perimeter of Rectangle:",perimeter)"""
+
+#Q8
+"""a = 5
+b = 6
+c = 7
+perimeter = a+b+c
+print("Perimeter of Triangle:",perimeter)"""
+
+#Q9
+"""side = 4
+area = side*side
+perimeter = 4*side
+print("Area of Square:",area)
+print("Perimeter of Square:",perimeter)"""
+
+#Q10
+side = 6
+perimeter = 4*side
+print("Perimeter of Square:",perimeter)
