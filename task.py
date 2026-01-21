2772ddf8c0d11c35c44c113f976b0dac4f70e12b
tarunmakwana2211 <tarunmakwana0001@gmail.com>
2026-01-21 08:27:10 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,55 @@
+#Q1 
+"""p = float(input("Enter principal amount:"))
+r = float(input("Enter rate os interest:"))
+t = float(input("Enter time (in years):"))
+si = (p*r*t) / 100
+print("Simple Interest =",si)"""
+
+#Q2
+"""for i in range(1,6):
+    print(i)"""
+    
+#Q3
+"""a =int(input("Enter first number:"))
+b =int(input("Enter second number:"))
+if a>b:
+     print("maximum number is:",a)
+else:
+     print("maximum number is:",b)"""
+     
+#Q4
+"""s = input("Enter a string:")
+print("Length of string:",len(s))"""
+
+#Q5
+"""name = input("Enter your name:")
+print("welcome,",name)"""
+
+#Q6
+"""s=input("Enter a string:")
+print("First character:",s[0])"""
+
+#Q7
+"""s=input("Enter a string:")
+print("Last character:",s[-1])"""
+
+#Q8
+"""num=int(input("Enter a number:"))
+if num>=0:
+    print("Positive number")
+else:
+    print("Negative number")"""
+     
+#Q9
+"""a =int(input("Enter first number:"))
+b =int(input("Enter second number:"))
+c =int(input("Enter third number:"))
+sum=a+b+c
+print("Sum=",sum)"""
+
+#Q10
+num=int(input("Enter a number:"))
+if num %2==0:
+    print("The number is Even")
+else:
+    print("The number is odd")
