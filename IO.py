43e1132780199bb5447711fe75b4c720a01e1457
tarunmakwana2211 <tarunmakwana0001@gmail.com>
2026-02-02 10:35:38 +0530

    Add files via upload

---
+++ IO.py
@@ -0,0 +1,49 @@
+#OUTPUT using print()
+
+#1. int example
+"""a = 22
+print(a)
+b = 33
+print(b)"""
+
+#2. float example
+"""b = 3.3
+print(b)
+c = 3.32
+print(c)"""
+
+#3. bool example
+"""x = True
+print(x)
+x = False
+print(x)"""
+
+#4. str example
+"""x = "hello america"
+print(x)
+x = "good morning"
+print(x)"""
+
+#INPUT using input()
+
+#1. int example
+"""number = int(input("enter number:"))
+print("number is:",number)
+number = int(input("enter number:"))
+print("number is:",number)"""
+
+#2. float example
+"""price = float(input("enter price:"))
+print("price is:",price)
+price = float(input("enter price:"))
+print("price is:",price)"""
+
+#3. bool example
+"""Q = bool(input("this is true"))
+print(Q)
+Q = bool(input("this is False"))
+print(Q)"""
+
+#4. str example
+name = str(input("enter your name:"))
+print("hello",name)
