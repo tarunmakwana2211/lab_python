683d1baa9d9ad4b28fbffb1a3e622bdeb49d324d
tarunmakwana2211 <tarunmakwana0001@gmail.com>
2026-02-09 10:11:32 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,45 @@
+#age
+"""age = int(input("Enter your age:"))
+if age >= 20:
+    print("you are eligible")
+else:
+    print("you are not eligible")"""
+
+#marks
+"""marks = int(input("enter your marks:"))
+if marks >=90:
+    print("you get A grade")
+elif marks>=70:
+    print("you get B grade")
+else:
+    print("you get C grade")"""
+    
+#odd even
+"""num = int(input("Enter number:"))
+if num%2==0:
+    print("even number")
+else:
+    print("odd number")"""
+    
+#positive negative 
+"""num = int(input("Enter a number:"))
+if num>0:
+    print("Positive number")
+elif num<0:
+    print("Negative number") 
+else:
+    print("Zero") """
+ 
+#vote 
+"""age = int(input("Enter your age:"))
+if age >= 20:
+    print("you are eligible for vote")
+else:
+    print("you are not eligible for vote")"""
+    
+#vote 
+age = int(input("Enter your age:"))
+if age >= 20:
+    print("you are eligible for licence")
+else:
+    print("you are not eligible for licence")
