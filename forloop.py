396cbdebcf58306b58cf3d0111d68a6b6f166fb0
tarunmakwana2211 <tarunmakwana0001@gmail.com>
2026-02-16 10:25:56 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,41 @@
+#example
+#i)
+"""for i in range(1,6):
+    print(i)"""
+#ii)            
+"""for i in range(3):
+    print("Hello")"""
+    
+#Q1
+"""for i in range (1,11):
+    print(i)"""
+
+#Q2
+"""for i in range (1,21):
+    if i%2==0:
+        print(i)"""
+        
+#Q3
+"""for i in range (1,16):
+    if i%2 !=0:
+        print(i)"""
+
+#Q4
+"""for i in range (1,11):
+    print("5 x",i,"=",5*i)"""
+    
+#Q5
+"""name ="atmiya"
+for letter in name:
+    print(letter)"""
+    
+#Q6
+"""total=0
+for i in range (1,6):
+    total=total+1
+    print("Sum is:",total)"""
+    
+#Q7
+number=[10,20,30,40]
+for n in number:
+    print(n)
