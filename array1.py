cf19022e177605a1dd190c2560621d4a7a3cf4d5
tarunmakwana2211 <tarunmakwana0001@gmail.com>
2026-03-11 09:15:16 +0530

    Add files via upload

---
+++ array1.py
@@ -0,0 +1,47 @@
+# ARRAY OPERATION
+#Q1
+"""from array import array
+arr = array('i',[10,20,30,40,50])
+print(len(arr))"""
+
+#Q2
+"""from array import array
+arr = array('i',[10,20,30])
+arr.append(40)
+print(arr)"""
+
+#Q3
+"""from array import array
+arr = array('i',[10,20,40])
+arr.insert(2,30)
+print(arr)"""
+
+#Q4
+"""from array import array
+arr = array('i',[10,20,30,20,40])
+arr.remove(20)
+print(arr)"""
+
+#Q5
+"""from array import array
+arr = array('i',[10,20,30,40])
+x=arr.pop()
+print("Remove:",x)
+print(x)"""
+
+#Q6
+"""from array import array
+arr = array('i',[10,20,30,40])
+print(arr.index(30))"""
+
+#Q7
+"""from array import array
+arr = array('i',[10,20,30,20,40])
+print(arr.count(20))"""
+
+#Q8
+from array import array
+arr = array('i',[10,20,30,40])
+arr.reverse()
+print(arr)
+
