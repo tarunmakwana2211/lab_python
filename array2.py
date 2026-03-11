c6b5a7304ec3e164f3723ea64b6497d09f3dcc11
tarunmakwana2211 <tarunmakwana0001@gmail.com>
2026-03-11 09:15:32 +0530

    Add files via upload

---
+++ array2.py
@@ -0,0 +1,15 @@
+# INDEXING ARRAY
+
+#Q1
+"""from array import array
+arr = array('i',[10,20,30,40,50])
+print(arr[0])
+print(arr[2])
+print(arr[4])"""
+
+#Q2
+from array import array
+arr = array('i',[10,20,30,40,50])
+print(arr[-1])
+print(arr[-2])
+print(arr[-5])
