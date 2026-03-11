ad6d9d870f2bbe394c53d963569bf32982596f18
tarunmakwana2211 <tarunmakwana0001@gmail.com>
2026-03-11 09:16:28 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,39 @@
+# 1
+"""i = 1
+while i<=5:
+    print(i)
+    i=i+1"""
+    
+# 2
+"""n1 = int(input("Enter number:"))
+n2 = int(input("Enter number:"))
+while n1+n2:
+    print("Sum:",n1+n2)
+    break"""
+# 3
+"""for i in range(1,20):
+   if i%2 !=0:
+       print(i)"""
+       
+# 4
+"""n = 4
+i = 1
+while i <=10:
+    print(n,"x",i,"=",n*i)
+    i=i+1"""
+    
+# 5
+"""i = 10
+while i > 0:
+    print(i)
+    i=i-1"""
+    
+# 6
+"""n = [1000,2000,3000,4000,5000]
+largest = max(n) 
+print(largest)"""
+
+# 7
+"""for i in range(1,20):
+   if i%2 ==0:
+       print(i)"""
