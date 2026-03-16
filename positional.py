ee593d91eed46593f06a0779c6e029917b2d25f7
tarunmakwana2211 <tarunmakwana0001@gmail.com>
2026-03-16 10:41:08 +0530

    Add files via upload

---
+++ positional.py
@@ -0,0 +1,56 @@
+#Q1
+"""def add(a,b):
+    print("a=",a)
+    print("b=",b)
+    return a+b
+result=add(2,5)
+print("sum=",result)"""
+
+#Q2
+"""def student_info(name,roll,marks):
+    print("name:",name)
+    print("roll no:",roll)
+    print("marks:",marks)
+student_info("ravi",101,85)"""
+
+#simple interest
+"""def simple_interest(p,r,n):
+    si=(p*r*n)/100
+    print("simple interest:",si)
+simple_interest(10000,2,2)
+simple_interest(50000,1.2,3)"""
+
+#Area of circle
+"""def ar_circle(r):
+    a_circle=3.14*r*r
+    print("Area of circle:",a_circle)
+ar_circle(1.5)
+ar_circle(4)"""
+
+#check number positive negative or zero
+"""def check_value(no):
+    if(no>0):
+        print("Positive")
+    elif(no<0):
+        print("Negative")
+    else:
+        print("Zero")
+check_value(0)
+check_value(90)
+check_value(-15)"""
+
+#odd or even
+"""def odd_even(no):
+    if(no%2==0):
+        print(f"value{no}in even")
+    else:
+        print(f"value{no}is odd")
+odd_even(50)
+odd_even(15)"""
+
+#arithmatic operation subtraction multiplication and division
+def addition(a,b):
+    add=a+b
+    print("Addition of two value",add)
+addition(50,10.5)
+addition(100,200)
