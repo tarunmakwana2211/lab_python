3a4ff4c6486fb08a7a963fa77e07fdfb4876e9e9
tarunmakwana2211 <tarunmakwana0001@gmail.com>
2026-03-16 10:40:23 +0530

    Add files via upload

---
+++ default.py
@@ -0,0 +1,18 @@
+#Q1
+"""def sqr(num,exp=2):
+    return num**exp
+print(sqr(3))
+print(sqr(3,3))
+print(sqr(2,4))"""
+
+#Q2
+"""def greet(name="guest"):
+    print("hello",name)
+greet("alice")
+greet()"""
+
+#Q3
+def add(a,b=5):
+    print("sum:",a+b)
+add(10,20)
+add(10)
